4320e96979185b48bde78a96d178936946086273
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2022-12-18 17:27:08 +0700

    Add files via upload

---
+++ src/uappbo/ProductCounter.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package uap_pbo;
+
+/**
+ *
+ * @author J I H A N
+ */
+public interface ProductCounter {
+    public final double TAX = 0.2;
+ 
+    public int hitungJumlahProduk();
+    
+    public int hitungHargaProduk();
+}
+
