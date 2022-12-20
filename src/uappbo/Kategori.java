3a73775928c7d89f6d2645cd6c3f765adccd347c
Stefanus Ferry Susanto <91201205+StfnsFerry@users.noreply.github.com>
2022-12-20 09:54:02 +0700

    Update Kategori.java

--- src/uappbo/Kategori.java
+++ src/uappbo/Kategori.java
@@ -1,21 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package uap_pbo;
+package uappbo;
 
-/**
- *
- * @author J I H A N
- */
 public class Kategori {
     private Produk produk  = new Produk();
     int id_kategori;
     private String nama_kategori;
 
-    public Kategori(int id_kategori, String nama_kategori) {
-        this.id_kategori = id_kategori;
+    public Kategori(String nama_kategori) {
         this.nama_kategori = nama_kategori;
+    }
+  
+    
+    public Kategori(int id_kategori,String nama_kategori) {
+        this.nama_kategori = nama_kategori;
+        this.id_kategori = id_kategori;
     }
 
     public Kategori() {
@@ -45,4 +42,3 @@ public class Kategori {
         this.nama_kategori = nama_kategori;
     }
 }
-
